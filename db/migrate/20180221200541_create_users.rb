104cab8ab1fd46f946c460b22842bdf91751bb69
Ty Thompson <tylermoko@gmail.com>
2018-02-21 15:26:14 -0500

    migrate tables

--- db/migrate/20180221200541_create_users.rb
+++ db/migrate/20180221200541_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :fname
+      t.string :lname
+      t.string :username
+    	t.string :email
+    	t.string :password_digest
       t.timestamps null: false
     end
   end
--- db/migrate/20180221200549_create_posts.rb
+++ db/migrate/20180221200549_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.string :title
+      t.string :content
+      t.integer :user_id
       t.timestamps null: false
     end
   end
