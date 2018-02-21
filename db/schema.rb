7a852693f576a0637c7a5911bb96df3c9d0649f3
Ty Thompson <tylermoko@gmail.com>
2018-02-21 15:43:53 -0500

    fixed table and tested index page

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221200555) do
+ActiveRecord::Schema.define(version: 20180221203456) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
-    t.integer  "blog_id"
+    t.integer  "post_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
