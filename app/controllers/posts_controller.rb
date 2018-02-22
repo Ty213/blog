c16a9222446b0ed4ddc9f1fe710a5d6f526537f8
Ty Thompson <tylermoko@gmail.com>
2018-02-22 11:45:52 -0500

    added filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,8 @@
 class PostsController < ApplicationController
+# before_action :authenticate, only: [:index]
+  before_action :authenticate
+    def index
+        @posts = Post.all
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root to: "users#index"
+  root to: "sessions#new"
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 
   resources :users
-  resources :blogs
+  resources :posts do
+    resources :comments
+  end
 
 end
