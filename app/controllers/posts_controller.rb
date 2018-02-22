3e9b211bb9f00b32a6e5138caf35932264b6ec95
Ty Thompson <tylermoko@gmail.com>
2018-02-22 12:18:10 -0500

    adding posts pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,25 @@ class PostsController < ApplicationController
         @posts = Post.all
     end
 
+    def new
+        @post = Post.new
+    end
+
+    def create
+		post = Post.new(post_params)
+		if post.save
+			flash[:message] = 'Your post was created successfully'
+			redirect_to '/posts'
+		else
+			flash[:message] = 'try again'
+			render '/posts/new'
+		end
+    end
+    
+    private
+
+def post_params
+	params.require(:post).permit(:title, :content)
+end
+
 end
