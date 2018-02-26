3dc0e66550717e8003893d4a14b1eddbba320cde
Ty Thompson <tylermoko@gmail.com>
2018-02-26 07:47:51 -0500

    adding javascript.

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,7 @@
+console.log("tesfkreopkgopekgopert");
+
+commentButton = document.querySelector('.commentsButton');
+
+commentButton.addEventListener(type, function () {
+    console.log('testing');
+});
